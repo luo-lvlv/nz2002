eb76d2aa52f1a68424f74e06170af7a194e4bf50
luo-lvlv <yu_comon@163.com>
2020-08-25 20:32:36 +0800

    man做的工作

---
+++ src/components/button/button.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+import './button.css'
+export default class Button extends Component{
+  render(){
+    return (
+      <div className="button">
+        <h3>button</h3>
+      </div>
+    )
+  }
+}
